33d1f984ed33cf29a5429ba7b69828dbaaa8f59c
Ben Greear <greearb@candelatech.com>
2016-02-11 18:52:18 -0800

    ath10k: Start supporting customization for 10.4 CT firmware.
    This adds the framework to take the vdev-count, peer-count,
    and other config from module-params and configure the 10.4
    firmware.  Some of this will only work with CT 10.4 firmware.
    
    Also, if we get WMI timeouts, pretend the firmware has crashed,
    attempt to gather logs, and then restart the firmware.  Hopefully
    the bug I found in 10.4 is now fixed in the CT 10.4 firmware
    and this code will not actually be needed anymore!
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/mac.c
+++ drivers/net/wireless/ath/ath10k/mac.c
@@ -9042,8 +9042,7 @@ int ath10k_mac_register(struct ath10k *ar)
 	case ATH10K_FW_WMI_OP_VERSION_10_2_4:
 		if (test_bit(ATH10K_FW_FEATURE_WMI_10X_CT,
 			     ar->running_fw->fw_file.fw_features)) {
-			ath10k_10x_ct_if_comb[0].limits[0].max =
-				ar->max_num_vdevs;
+			ATH_ASSIGN_CONST_U16(ath10k_10x_ct_if_comb[0].limits[0].max, ar->max_num_vdevs);
 			ath10k_10x_ct_if_comb[0].max_interfaces =
 				ar->max_num_vdevs;
 
@@ -9067,12 +9066,11 @@ int ath10k_mac_register(struct ath10k *ar)
 	case ATH10K_FW_WMI_OP_VERSION_10_4:
 		if (test_bit(ATH10K_FW_FEATURE_WMI_10X_CT,
 			     ar->running_fw->fw_file.fw_features)) {
-			ath10k_10_4_ct_if_comb[0].limits[0].max =
-				ar->max_num_vdevs;
+			ATH_ASSIGN_CONST_U16(ath10k_10_4_ct_if_comb[0].limits[0].max, ar->max_num_vdevs);
 			ath10k_10_4_ct_if_comb[0].max_interfaces =
 				ar->max_num_vdevs;
 			if (ath10k_10_4_ct_if_comb[0].limits[1].max > ar->max_num_vdevs)
-				ath10k_10_4_ct_if_comb[0].limits[1].max = ar->max_num_vdevs;
+				ATH_ASSIGN_CONST_U16(ath10k_10_4_ct_if_comb[0].limits[1].max, ar->max_num_vdevs);
 
 			ar->hw->wiphy->iface_combinations =
 				ath10k_10_4_ct_if_comb;

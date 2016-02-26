4a2015a5ac60bea0170e688e8f8f278fc7c94cda
Ben Greear <greearb@candelatech.com>
2016-02-26 12:20:17 -0800

    ath10k: show firmware directory and name in debugfs.
    This lets one know what firmware a particular radio is using:
    
    dmesg:
    
        [14038.475616] ath10k_pci 0000:05:00.0: using fw api 2: ath10k/QCA988X/hw2.0/firmware-2.bin
        [14038.519826] ath10k_pci 0000:07:00.0: using fw api 2: ath10k/QCA988X/hw2.0/firmware-2-htt-mgt.bin
    
        directory: ath10k/QCA988X/hw2.0
        firmware:  firmware-2-htt-mgt.bin
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/core.c
+++ drivers/net/wireless/ath/ath10k/core.c
@@ -1451,6 +1451,10 @@ out:
 		goto err;
 	}
 
+	/* Save firmware name so we can display it later. */
+	strlcpy(ar->normal_mode_fw.fw_file.fw_name, filename,
+		sizeof(ar->normal_mode_fw.fw_file.fw_name));
+
 	return 0;
 
 err:
@@ -1750,6 +1754,9 @@ fw_ie_bss_info_ct:
 				  fw_file->fw_features);
 	}
 
+	/* Save firmware name so we can display it later. */
+	strlcpy(fw_file->fw_name, name, sizeof(fw_file->fw_name));
+
 	return 0;
 
 err:
@@ -1819,7 +1826,9 @@ static int ath10k_core_fetch_firmware_files(struct ath10k *ar)
 	return ret;
 
 success:
-	ath10k_dbg(ar, ATH10K_DBG_BOOT, "using fw api %d\n", ar->fw_api);
+	ath10k_dbg(ar, ATH10K_DBG_BOOT, "using fw api %d: %s/%s\n",
+		   ar->fw_api, ar->hw_params.fw.dir,
+		   ar->normal_mode_fw.fw_file.fw_name);
 
 	return 0;
 }

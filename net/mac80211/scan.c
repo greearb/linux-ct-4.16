8aa64a49f4695e091f7bce727f891593d6217a1e
Ben Greear <greearb@candelatech.com>
2016-06-07 16:09:00 -0700

    mac80211: scan: Modify chan settings if width is specified.
    If user specifies a particular bandwidth to scan at, then assume
    current operating channel may not be that width and bypass the
    scan-on-channel optimization.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- net/mac80211/scan.c
+++ net/mac80211/scan.c
@@ -640,6 +640,7 @@ static int __ieee80211_start_scan(struct ieee80211_sub_if_data *sdata,
 	if (local->ops->hw_scan) {
 		__set_bit(SCAN_HW_SCANNING, &local->scanning);
 	} else if ((req->n_channels == 1) &&
+		   (req->scan_width == 0) &&
 		   (req->channels[0] == local->_oper_chandef.chan)) {
 		/*
 		 * If we are scanning only on the operating channel

94b586bc8e0858b92bc971594960209fff8178ca
Ben Greear <greearb@candelatech.com>
2018-02-07 13:32:36 -0800

    ath10k:  Fix deadlock due to scan-stop logic.
    We cannot actually call WMI commands under lock, so change
    the scan-stop work-around to use the work-queue instead of
    a direct call.  This fixes an earlier bug introduced by me,
    and the original did not ever make it upstream.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/wmi.h
+++ drivers/net/wireless/ath/ath10k/wmi.h
@@ -6998,6 +6998,7 @@ void ath10k_wmi_10_4_op_fw_stats_fill(struct ath10k *ar,
 int ath10k_wmi_op_get_vdev_subtype(struct ath10k *ar,
 				   enum wmi_vdev_subtype subtype);
 int ath10k_wmi_barrier(struct ath10k *ar);
+void ath10k_wmi_stop_scan_work(struct work_struct *work);
 
 #ifdef CONFIG_ATH10K_DEBUGFS
 /* TODO:  Should really enable this all the time, not just when DEBUGFS is enabled. --Ben */
